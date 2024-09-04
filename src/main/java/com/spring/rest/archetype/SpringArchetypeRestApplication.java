756674536e519b61d647289b84f4de72a77ac3bb
Jorge Cañas <jorcanha@users.noreply.github.com>
2024-09-04 10:25:12 -0500

    initial commit spring boot project

---
+++ src/main/java/com/spring/rest/archetype/SpringArchetypeRestApplication.java
@@ -0,0 +1,13 @@
+package com.spring.rest.archetype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringArchetypeRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringArchetypeRestApplication.class, args);
+	}
+
+}
